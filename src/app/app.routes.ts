6f1cb31d68ab658719144a2137a425ed43a6ab7e
Andres Canevello <canevelo0o@gmail.com>
2024-11-10 18:41:31 -0300

    generamos la ruta admin y home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './web/components/home/home.component';
+import { MenuAdminComponent } from './admin/components/menu-admin/menu-admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'hp-admin',
+        component:MenuAdminComponent
+    }
+];
