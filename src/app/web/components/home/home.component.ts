5dc66ade0bf4dfa87aded65b7e3f13aeba90b60b
Andres Canevello <canevelo0o@gmail.com>
2024-11-10 03:09:15 -0300

    Creamos los compoonentes
    Creamos los componentes necesarios para las paginas en general

--- src/app/web/components/home/home.component.ts
+++ src/app/web/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
+import { HeaderComponent } from '../header/header/header.component';
+import { MainComponent } from '../main/main/main.component';
+import { FooterComponent } from '../footer/footer.component';
+import { AsideComponent } from '../aside/aside/aside.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent, HeaderComponent, MainComponent, FooterComponent, AsideComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
