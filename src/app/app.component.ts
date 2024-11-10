5c5212758bd1252303559eceac6ec603fb4b489a
Andres Canevello <canevelo0o@gmail.com>
2024-11-10 18:43:15 -0300

    integramos el menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './web/components/home/home.component';
+import { MenuAdminComponent } from './admin/components/menu-admin/menu-admin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet,HomeComponent,MenuAdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
