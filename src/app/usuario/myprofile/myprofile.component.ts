87eb02908ea6232b4689508b2f863fcf35f85a59
Gonzalo de Andrade <gonzadeandrade1@gmail.com>
2024-11-11 11:25:51 -0300

    myprofile

--- src/app/usuario/myprofile/myprofile.component.ts
+++ src/app/usuario/myprofile/myprofile.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UsuarioServicioService } from '../service/usuario-servicio.service';
 import { inject } from '@angular/core';
+import { MascotaService } from '../../shared/mascota/service/mascota.service';
+import { solicitudMascota } from '../../shared/mascota/Interface/solicitudMascota.interface';
 @Component({
   selector: 'app-myprofile',
   standalone: true,
@@ -10,11 +12,17 @@ import { inject } from '@angular/core';
 })
 export class MyProfileComponent {
   userData: any;
-userService = inject(UsuarioServicioService);
-
+  mascotas: solicitudMascota[] = [];
+  userService = inject(UsuarioServicioService);
+  solicitudService = inject(MascotaService);
   ngOnInit() {
+      // Obtener los datos del usuario
       this.userService.getUsuario().subscribe(data => {
           this.userData = data;
       });
+
+      // Obtener las solicitudes de mascota del usuario
+     
   }
+  
 }
