b215c42cc642587535db724e08d8fb60ffac5433
Andres Canevello <canevelo0o@gmail.com>
2024-11-11 03:21:35 -0300

    cambios en enviroment y bd

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,9 @@
 export const environment = {
-    urlBaseStandBySA: 'http://localhost:3000/mascotas/SA_stand_by',
-    urlBaseAceptadasSA: 'http://localhost:3000/mascotas/SA_aceptadas',
-    urlBaseRechazadasSA: 'http://localhost:3000/mascotas/SA_rechazadas',
-    urlBaseStandBySM: 'http://localhost:3000/mascotas/SM_stand_by',
-    urlBaseAceptadasSM: 'http://localhost:3000/mascotas/SM_aceptadas',
-    urlBaseRechazadasSM: 'http://localhost:3000/mascotas/SM_rechazadas',
+    urlBaseMascotas:'http://localhost:3000/mascotas',
+    urlBaseStandBySA: 'http://localhost:3000/SA_stand_by',
+    urlBaseAceptadasSA: 'http://localhost:3000/SA_aceptadas',
+    urlBaseRechazadasSA: 'http://localhost:3000/SA_rechazadas',
+    urlBaseStandBySM: 'http://localhost:3000/SM_stand_by',
+    urlBaseAceptadasSM: 'http://localhost:3000/SM_aceptadas',
+    urlBaseRechazadasSM: 'http://localhost:3000/SM_rechazadas',
 };
