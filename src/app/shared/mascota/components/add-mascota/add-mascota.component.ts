2948027ebf186a24a49d3a4d7c13121c7fcd96cc
Andres Canevello <canevelo0o@gmail.com>
2024-11-10 22:11:22 -0300

    Creacion de componentes
    Creamos los componentes compartidos de MASCOTA

---
+++ src/app/shared/mascota/components/add-mascota/add-mascota.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-mascota',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-mascota.component.html',
+  styleUrl: './add-mascota.component.css'
+})
+export class AddMascotaComponent {
+
+}
---
+++ src/app/shared/mascota/components/listar-mascotas/listar-mascotas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-mascotas',
+  standalone: true,
+  imports: [],
+  templateUrl: './listar-mascotas.component.html',
+  styleUrl: './listar-mascotas.component.css'
+})
+export class ListarMascotasComponent {
+
+}
