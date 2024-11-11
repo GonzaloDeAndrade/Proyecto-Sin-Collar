abd23e40fbf5fdb9fadb393e15f16fae7c7c3870
Andres Canevello <canevelo0o@gmail.com>
2024-11-11 05:28:27 -0300

    cambios

--- src/app/shared/mascota/Interface/solicitudMascota.interface.ts
+++ src/app/shared/mascota/Interface/solicitudMascota.interface.ts
@@ -1,10 +1,12 @@
 export interface solicitudMascota{
     id:string,
+    id_Usuario:string,
     nombre: string,
     raza:string,
     edad:number,
     sexo:string,
     tamanio: number,
     color:string,
-    resultado?:boolean
+    resultado?:boolean,
+    imagen:string
 }
