9bb3d73458988dffa478cfe014d17bac1a8ef55c
Andres Canevello <canevelo0o@gmail.com>
2024-11-11 03:27:56 -0300

    creacion componente solicitudes mascota

---
+++ src/app/shared/mascota/pages/solicitudes-mascota/solicitudes-mascota.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitudes-mascota',
+  standalone: true,
+  imports: [],
+  templateUrl: './solicitudes-mascota.component.html',
+  styleUrl: './solicitudes-mascota.component.css'
+})
+export class SolicitudesMascotaComponent {
+
+}
