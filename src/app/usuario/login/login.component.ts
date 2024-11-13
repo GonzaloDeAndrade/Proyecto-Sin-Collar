aa8efc2972b0eabd85720a0d56a218095f5bb3cc
Gonzalo de Andrade <gonzadeandrade1@gmail.com>
2024-11-13 04:32:01 -0300

    login compomnent añadido

---
+++ src/app/usuario/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../../web/components/nav/nav.component';
+import { FormBuilder,FormGroup,Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { UsuarioServicioService } from '../service/usuario-servicio.service';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [NavComponent,ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  private formBuilder: FormBuilder = inject(FormBuilder)
+  private auth: UsuarioServicioService = inject(UsuarioServicioService)
+  private router: Router = inject(Router)
+
+  formulario: FormGroup = this.formBuilder.group({
+    email: ['', Validators.required],
+    contraseña: ['', Validators.required],
+  })
+
+  iniciarSession() {
+    if (this.formulario.invalid) return;
+
+    this.auth.verificarUserAndPass(
+      this.formulario.controls['email'].value,
+      this.formulario.controls['contraseña'].value)
+
+
+    /* const user: User = {
+      user: this.formulario.controls['user'].value,
+      password: this.formulario.controls['password'].value,
+      id?:null
+    }
+
+    this.auth.loginUser(user.user, user.password) */
+  }
+}
+
--- src/app/usuario/myprofile/myprofile.component.ts
+++ src/app/usuario/myprofile/myprofile.component.ts
@@ -1,33 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UsuarioServicioService } from '../service/usuario-servicio.service';
 import { inject } from '@angular/core';
 import { MascotaService } from '../../shared/mascota/service/mascota.service';
 import { solicitudMascota } from '../../shared/mascota/Interface/solicitudMascota.interface';
-import { NavComponent } from "../../web/components/nav/nav.component";
-import { FooterComponent } from "../../web/components/footer/footer.component";
+import { FooterComponent } from '../../web/components/footer/footer.component';
+import { NavComponent } from '../../web/components/nav/nav.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-myprofile',
   standalone: true,
-  imports: [NavComponent, FooterComponent],
+  imports: [FooterComponent,NavComponent,CommonModule],
   templateUrl: './myprofile.component.html',
   styleUrl: './myprofile.component.css'
 })
-export class MyProfileComponent implements OnInit{
+export class MyProfileComponent {
   userData: any;
   mascotas: solicitudMascota[] = [];
   userService = inject(UsuarioServicioService);
-  solicitudService = inject(MascotaService);
+  mascotaService = inject(MascotaService);
   ngOnInit() {
       // Obtener los datos del usuario
+      this.obtenerMascotas();
       this.userService.getUsuario().subscribe(data => {
           this.userData = data;
       });
-
       // Obtener las solicitudes de mascota del usuario
      
   }
-  obtenerMascota(){
-    this.solicitudService.getMascotasUser()
+  obtenerMascotas() {
+    // Llama al servicio para obtener las mascotas del usuario
+    this.mascotaService.getMascotasUser().subscribe(
+        (mascotas) => {
+            this.mascotas = mascotas;
+        },
+        (error) => {
+            console.error("Error al obtener las mascotas:", error);
+        }
+    );
   }
+
   
 }
