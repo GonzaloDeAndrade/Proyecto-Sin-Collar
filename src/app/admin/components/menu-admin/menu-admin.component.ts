b84d11f0c25121b0d847547c2855b7c4da08fd22
Andres Canevello <canevelo0o@gmail.com>
2024-11-20 01:09:53 -0300

    cambioos

--- src/app/admin/components/menu-admin/menu-admin.component.ts
+++ src/app/admin/components/menu-admin/menu-admin.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MascotaPagesComponent } from '../../../shared/mascota/pages/mascota-pages/mascota-pages.component';
 import { UpdateMascotaPagesComponent } from '../../../shared/mascota/pages/update-mascota-pages/update-mascota-pages.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu-admin',
   standalone: true,
-  imports: [MascotaPagesComponent,UpdateMascotaPagesComponent],
+  imports: [MascotaPagesComponent,UpdateMascotaPagesComponent,CommonModule],
   templateUrl: './menu-admin.component.html',
   styleUrl: './menu-admin.component.css'
 })
 export class MenuAdminComponent {
-      
+    router = inject(Router)
+    volverMenu(){
+      this.router.navigate(['/home']);
+    }
+     
 }
